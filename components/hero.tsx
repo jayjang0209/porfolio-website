71ba2fb6c2b69fd5968dfd91aabd47be4ee5622a
jayjang0209 <jayjang0209@gmail.com>
2024-01-05 23:08:43 -0800

    add max width and padding to text

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,9 +10,9 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+    <section className="max-w-6xl w-full py-12 md:py-24 lg:py-32">
       <div className="container mx-auto">
-        <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+        <div className="grid gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_500px]">
           <img
             alt="Profile"
             className="mx-auto aspect-content overflow-hidden rounded-xl object-center lg:w-[80%] lg:order-last lg:aspect-square"
@@ -21,7 +21,7 @@ export default function Hero() {
           />
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+              <h1 className="pt-3 text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
                 Hello, I'm <span className="text-primary">Jay</span>
               </h1>
               <p className="max-w-[600px] text-zinc-500 md:text-xl dark:text-zinc-400">
