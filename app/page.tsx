444458a92f26a6acd201d8cf2f1dbdc1635865f0
jayjang0209 <jayjang0209@gmail.com>
2024-01-05 23:39:13 -0800

    implement about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import Hero from '@/components/hero'
+import SectionDivider from '@/components/section-divider'
+import About from '@/components/about'
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Hero />
+      <SectionDivider />
+      <About />
     </main>
   )
 }
