cfd3d890f1407f35b0ff1d6facdfd5dbb60de556
jayjang0209 <jayjang0209@gmail.com>
2024-01-05 22:52:14 -0800

    add socials const

--- lib/data.ts
+++ lib/data.ts
@@ -24,3 +24,18 @@ export const links = [
     hash: "#contact",
   },
 ] as const;
+
+export const socials = [
+  {
+    name: "Github",
+    url: "https://github.com/jayjang0209",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/jayjjang/",
+  },
+  {
+    name: "Resume",
+    url: "https://docs.google.com/document/d/11i0lw4HE6NbtCFTDUtpj5ZYzCPeOjGT88Q2_l7ZrxUM/edit?usp=sharing",
+  },
+] as const;
