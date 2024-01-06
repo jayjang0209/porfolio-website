82676adfa6bb4d8b32c8f23678b1a8499506fb97
jayjang0209 <jayjang0209@gmail.com>
2024-01-05 23:36:47 -0800

    implement section divider

---
+++ components/section-divider.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SectionDivider() {
+  return (
+    <div
+      className="bg-indigo-200 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
+    ></div>
+  );
+}
