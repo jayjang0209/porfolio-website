72ca3ba4c5f8e8a85b06f26fa793d592d036ac82
jayjang0209 <jayjang0209@gmail.com>
2024-01-08 14:10:51 -0800

    add img

--- components/about.tsx
+++ components/about.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export default function About() {
   return (
     <section
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="mt-24 mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       id="about"
     >
       <h1 className="text-4xl font-bold mb-5">About Me</h1>
--- components/project.tsx
+++ components/project.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { projectsData } from '@/lib/data'
+import Image from 'next/image';
 
 type ProjectProps = (typeof projectsData)[number];
+
 
 export default function Project
 ({ title, description, tags,imageUrl, github, demo, video }: ProjectProps) {
   return (
     <div className='mb-3 sm:mb-8 last:mb-0'>
-      <section className='bg-gray-100 max-w-[50rem] border border-black/5 overflow-hidden'>
-        <div className='flex flex-col h-full pt-4 pb-7 px-5'>
+      <section className='relative bg-gray-50 max-w-[40rem] border border-black/5 overflow-hidden sm:h-[21rem] hover:bg-gray-200 transition sm:group-even:pl-8 cursor-pointer'>
+        <div className='flex flex-col h-full pt-3 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%]'>
           <h3 className='text-2xl font-semibold text-zinc-800'>
             {title}
           </h3>
@@ -26,8 +28,45 @@ export default function Project
               </li>
             ))}
           </ul>
-              
+          
+          {/* {github && (
+            <a
+              className='inline-flex items-center gap-1 mt-4 text-sm font-medium text-zinc-500 hover:text-zinc-600'
+              href={github}
+              target='_blank'
+            >
+              <FaGithub />
+              <span>View Code</span>
+            </a>
+          )}
+          {demo && (
+            <a
+              className='inline-flex items-center gap-1 mt-4 text-sm font-medium text-zinc-500 hover:text-zinc-600'
+              href={demo}
+              target='_blank'
+            >
+              <FaExternalLinkAlt />
+              <span>View Demo</span>
+            </a>
+          )}
+          {video && (
+            <a
+              className='inline-flex items-center gap-1 mt-4 text-sm font-medium text-zinc-500 hover:text-zinc-600'
+              href={video}
+              target='_blank'
+            >
+              <FaYoutube />
+              <span>View Video</span>
+            </a>
+          )}           */}
         </div>
+
+        <Image
+          alt={title}
+          quality={95}
+          src={imageUrl}
+          className='absolute hidden sm:block top-8 -right-5 w-[15rem] rounded-t-lg shadow-2xl'
+         /> 
       </section>
     </div>
   )
