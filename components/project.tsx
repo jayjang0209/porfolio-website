2e93dca443df04ccd986efcfdc94a8785229975c
jayjang0209 <jayjang0209@gmail.com>
2024-01-08 13:47:01 -0800

    implement tech tags

--- components/project.tsx
+++ components/project.tsx
@@ -4,7 +4,7 @@ import { projectsData } from '@/lib/data'
 type ProjectProps = (typeof projectsData)[number];
 
 export default function Project
-({ title, description, imageUrl, github, demo, video }: ProjectProps) {
+({ title, description, tags,imageUrl, github, demo, video }: ProjectProps) {
   return (
     <div className='mb-3 sm:mb-8 last:mb-0'>
       <section className='bg-gray-100 max-w-[50rem] border border-black/5 overflow-hidden'>
@@ -15,6 +15,18 @@ export default function Project
           <p className='mt-2 leading-relaxed text-zinc-600'>
             {description}
           </p>
+
+          <ul className='flex flex-row flex-wrap gap-2 mt-4 sm:mt-auto'>
+            {tags.map((tag, index) => (
+              <li
+                className='px-2 py-1 text-[0.7rem] tracking-wider font-semibold text-white bg-indigo-500 rounded-md'
+                key={index}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+              
         </div>
       </section>
     </div>
