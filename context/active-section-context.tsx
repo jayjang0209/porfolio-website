ca1ee4e7f40615a486ba09e4870d9759a2489f1c
jayjang0209 <jayjang0209@gmail.com>
2024-01-08 20:27:19 -0800

    implement useActiveSectionContext fuc

---
+++ context/active-section-context.tsx
@@ -0,0 +1,32 @@
+import React, {useState, useContext, createContext} from "react";
+import type { SectionName } from "@/types/data/SectionName";
+import type { ActiveSectionContextProviderProps } from "@/types/context/ActiveSectionContextProviderProps";
+import type { ActiveSectionContextType } from "@/types/context/ActiveSectionContextType";
+
+export const ActiveSectionContext =
+  React.createContext<ActiveSectionContextType | null>(null);
+export const defaultActiveSection = "Home";
+
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
+  const [activeSection, setActiveSection] =
+    useState<SectionName>(defaultActiveSection);
+  const [timeOfLastClick, setTimeOfLastClick] = useState<number>(0);
+
+  return <div>active-section-context</div>;
+}
+
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+  if (!context) {
+    throw new Error(
+      "useActiveSectionContext must be used within a ActiveSectionContextProvider"
+    );
+  }
+  return context;
+}
+
+
+
