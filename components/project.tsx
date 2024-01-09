845ee14eeddfadc0cc4e3023d6d2875d591b70e8
jayjang0209 <jayjang0209@gmail.com>
2024-01-09 12:01:30 -0800

    add project modal

--- components/project.tsx
+++ components/project.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState, useRef } from "react";
 import { projectsData } from "@/lib/data";
@@ -7,6 +7,7 @@ import { FaGithub, FaYoutube } from "react-icons/fa";
 import { RiPagesLine } from "react-icons/ri";
 import { motion, useScroll, useTransform } from "framer-motion";
 import YoutubeModal from "@/components/youtube-modal";
+import ProjectModal from "@/components/project-modal";
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -14,14 +15,17 @@ export default function Project({
   title,
   description,
   tags,
+  tasks,
   imageUrl,
   github,
   demo,
   video,
 }: ProjectProps) {
   const [showVideo, setShowVideo] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
 
   const toggleShowVideo = () => setShowVideo(!showVideo);
+  const toggleShowDetails = () => setShowDetails(!showDetails);
 
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -33,12 +37,15 @@ export default function Project({
   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   return (
-    <motion.div 
+    <motion.div
       ref={ref}
       style={{ scale: scaleProgess, opacity: opacityProgess }}
-    
-    className="group mb-3 sm:mb-8 last:mb-0">
-      <section className="relative rounded-xl bg-gray-50 max-w-[42rem] border border-black/5 overflow-hidden sm:h-[21rem] hover:bg-gray-200 transition cursor-pointer">
+      className="group mb-3 sm:mb-8 last:mb-0"
+    >
+      <section
+        onClick={toggleShowDetails}
+        className="relative rounded-xl bg-gray-50 max-w-[42rem] border border-black/5 overflow-hidden sm:h-[21rem] hover:bg-gray-200 transition cursor-pointer"
+      >
         <div className="flex flex-col h-full pt-3 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%]">
           <h3 className="flex text-2xl font-semibold text-zinc-800">{title}</h3>
           <div className="flex items-center justify-start gap-2 mt-1 mb-1">
@@ -75,7 +82,7 @@ export default function Project({
           <ul className="flex flex-row flex-wrap gap-2 mt-4 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
-                className="px-2 py-1 text-[0.6rem] tracking-widest uppercase font-semibold text-white bg-zinc-500 rounded-md"
+                className="px-2 py-1 text-[0.6rem] tracking-widest uppercase font-semibold text-white bg-zinc-500/70 rounded-md"
                 key={index}
               >
                 {tag}
@@ -91,8 +98,19 @@ export default function Project({
           className="absolute hidden sm:block top-8 -right-5 w-[15rem] rounded-xl shadow-2xl
           transition group-hover:scale-[1.03] group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:-rotate-2"
         />
+        {showDetails && (
+          <ProjectModal
+            title={title}
+            description={description}
+            tags={tags}
+            tasks={tasks}
+            onClose={toggleShowDetails}
+          />
+        )}
       </section>
-      {showVideo && <YoutubeModal videoUrl={video} onClose={() => setShowVideo(false)} />}
+      {showVideo && (
+        <YoutubeModal videoUrl={video} onClose={() => setShowVideo(false)} />
+      )}
     </motion.div>
   );
 }
