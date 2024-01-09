0d47aa9b718e4cb19fb51fab5491d5614fc28fc4
jayjang0209 <jayjang0209@gmail.com>
2024-01-08 16:52:19 -0800

    build skeleton for Experience

---
+++ components/experience.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Experience() {
+  return (
+    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
+      <h1 className="text-4xl font-bold mb-5 text-center">Experience</h1>
+
+    </section>
+  )
+}
