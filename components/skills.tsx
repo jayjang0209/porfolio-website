e715c6963333ff578d3fc392eba55af585e6bb0c
jayjang0209 <jayjang0209@gmail.com>
2024-01-08 16:52:32 -0800

    add id to skills section

--- components/skills.tsx
+++ components/skills.tsx
@@ -3,7 +3,9 @@ import { skillsData } from '@/lib/data'
 
 export default function Skills() {
   return (
-    <section className='mb-28 max-w-[50rem] scroll-mt-28 text-center sm:mb-40'>
+    <section className='mb-28 max-w-[50rem] scroll-mt-28 text-center sm:mb-40'
+      id='skills'
+    >
       <h1 className="text-4xl font-bold mb-5 text-center">Skills</h1>
       <div className='flex flex-wrap items-center justify-center gap-5'>
         {skillsData.map((skill, index) => (
