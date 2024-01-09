da44ab813affe1a75c52e2d3b7c81203c943db62
jayjang0209 <jayjang0209@gmail.com>
2024-01-09 14:48:37 -0800

    add error handler

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,17 +1,53 @@
-"use server"
+"use server";
 
-import { Resend } from "resend"
+import { Resend } from "resend";
+import { EmailLink } from "@/lib/data";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const validateEmail = (email: string) => {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
+
+const validateMessage = (message: string) => {
+  return message.length > 0;
+};
+
+const getErrorMesssage = (error: unknown) => {
+  if (error instanceof Error) {
+    return error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    return String(error.message);
+  } else {
+    return "Unknown error";
+  }
+}
 
 export const sendEmail = (formData: FormData) => {
-  console.log("senderEmail", formData.get("senderEmail"))
-  console.log("msg", formData.get("message"))
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
 
-  resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: 'jayjang0209@gmail.com',
-    subject: 'Mesaage from Portfolio Site',
-    html: `<p>${formData.get("message")}</p>`,
-  })
-}
+  console.log("senderEmail", senderEmail);
+  console.log("msg", message);
+
+  if (!validateEmail(senderEmail as string)) {
+    return new Error("Invalid Email");
+  }
+
+  if (!validateMessage(message as string)) {
+    return new Error("Invalid Message");
+  }
+
+  try {
+    resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: EmailLink,
+      subject: "Mesaage from Portfolio Site",
+      reply_to: senderEmail as string,
+      html: `<p>${message}</p>`,
+    });
+  } catch (error: unknown) {
+    return getErrorMesssage(error);
+  }
+};
