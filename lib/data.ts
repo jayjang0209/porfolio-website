6ca0aff569e8a14858ef12e6451921c3ed50769a
jayjang0209 <jayjang0209@gmail.com>
2024-01-08 19:10:58 -0800

    add email const

--- lib/data.ts
+++ lib/data.ts
@@ -43,6 +43,7 @@ export const GithubLink = "https://github.com/jayjang0209";
 export const LinkedInLink = "https://www.linkedin.com/in/jayjjang/";
 export const ResumeLink =
   "https://docs.google.com/document/d/11i0lw4HE6NbtCFTDUtpj5ZYzCPeOjGT88Q2_l7ZrxUM/edit?usp=sharing";
+export const EmailLink = "jayjang0209@gmail.com"
 
 export const projectsData = [
   {
