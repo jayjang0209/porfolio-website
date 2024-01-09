6d2b1537c4c35a443dfcc8867e93f335e9d1b5e8
jayjang0209 <jayjang0209@gmail.com>
2024-01-08 19:05:24 -0800

    Implemente VerticalTimeline

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
 
 export default function Experience() {
+
   return (
     <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
-      <h1 className="text-4xl font-bold mb-5 text-center">Experience</h1>
+      <h1 className="text-4xl font-bold mb-5 text-center">My Projects</h1>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              visible={true}
+              contentStyle={{
+                background: "#fff",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background: "#e5e7eb",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.place}</p>
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75 pb-4">
+                {item.description}
+              </p>
 
+              {item.details?.skills && (
+                <ul className="flex flex-row flex-wrap gap-2 mt-6 sm:mt-auto">
+                  {item.details.skills.map((skill, index) => (
+                    <li
+                      className="px-2 py-1 text-[0.7rem] tracking-wider font-semibold text-zinc-700 bg-indigo-200 rounded-full"
+                      key={index}
+                    >
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
+              )} 
+
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </section>
-  )
+  );
 }
