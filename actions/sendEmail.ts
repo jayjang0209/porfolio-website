1ab8461264301c19bb7616d5b5cf81671c837506
jayjang0209 <jayjang0209@gmail.com>
2024-01-09 14:23:55 -0800

    test sending email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,6 +1,17 @@
 "use server"
 
+import { Resend } from "resend"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
 export const sendEmail = (formData: FormData) => {
   console.log("senderEmail", formData.get("senderEmail"))
   console.log("msg", formData.get("message"))
+
+  resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: 'jayjang0209@gmail.com',
+    subject: 'Mesaage from Portfolio Site',
+    html: `<p>${formData.get("message")}</p>`,
+  })
 }
