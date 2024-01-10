af9aab2279aecbc2710e374829c252296c11c3d2
jayjang0209 <jayjang0209@gmail.com>
2024-01-09 18:31:58 -0800

    fix year

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col justify-center items-center gap-1 py-3 bg-indigo-600/80 text-white/80 dark:bg-opacity-40 dark:bg-black">
       <p className="text-sm">
-        &copy; 2023 Jay Jang. All rights reserved.
+        &copy; 2024 Jay Jang. All rights reserved.
       </p>
       <p className='text-xs text-center'>
         Built with <a className="text-primary underline" href="https://nextjs.org/">Next.js</a>{" "},
