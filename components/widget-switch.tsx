eb3fe0f35c7dbdb430941a775357e69c2b79f7bb
jayjang0209 <jayjang0209@gmail.com>
2024-01-09 16:39:22 -0800

    enable dark class

--- components/widget-switch.tsx
+++ components/widget-switch.tsx
@@ -1,23 +1,50 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
-import { PortfolioGihuLink } from '@/lib/data';
+import { PortfolioGihuLink } from "@/lib/data";
 
 export default function ThemeSwitch() {
- const [theme, setTheme] = React.useState("light");
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    if (localTheme) {
+      setTheme(localTheme);
+    } else if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      window.localStorage.setItem("theme", "light");
+    }
+  }, []);
 
   return (
-    <div className='flex flex-col fixed bottom-5 right-5 gap-2'>
+    <div className="flex flex-col fixed bottom-5 right-5 gap-2">
       <button
         className="bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
-        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        onClick={toggleTheme}
       >
         {theme === "light" ? <BsSun /> : <BsMoon />}
       </button>
       <a
-        className="bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+        className="text-[1.4rem] text-zinc-600 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
         href={PortfolioGihuLink}
         target="_blank"
       >
