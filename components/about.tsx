4768abf1eae56aac0c5a0b06044441e10e0b4507
jayjang0209 <jayjang0209@gmail.com>
2024-01-09 18:31:27 -0800

    add br tags

--- components/about.tsx
+++ components/about.tsx
@@ -25,16 +25,21 @@ export default function About() {
           {" "}
           British Columbia Institute of Technology
         </span>{" "}
-        with a passion for software innovation and cloud computing. At{" "}
-        <span className="highlight">SAP</span>, as an
+        with a passion for software innovation and cloud computing.
+        <br />
+        At <span className="highlight">SAP</span>, as an
         <span className="emphasis">Agile Developer Intern</span> in the platform
         infrastructure team, I engaged in developing microservices running on
         diverse cloud platforms and honed my skills in DevOps through updates
-        and troubleshooting of virtual Linux machines. During my tenure at
+        and troubleshooting of virtual Linux machines.
+        <br />
+        During my tenure at
         <span className="highlight"> IOTO</span>, I embraced the{" "}
         <span className="emphasis">full-stack developer</span>
         role, where I crafted sophisticated web scrapers, APIs, and user
-        interfaces from the ground up. I'm now seeking a{" "}
+        interfaces from the ground up. 
+        <br />
+        I'm now seeking a{" "}
         <span className="emphasis">full-time position</span> where I can apply
         my diverse skill set in a dynamic and innovative environment.
       </p>
