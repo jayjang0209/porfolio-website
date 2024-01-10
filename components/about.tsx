04a8085dedd4f7ac3df221321c4bf50697c8f85b
jayjang0209 <jayjang0209@gmail.com>
2024-01-09 18:28:37 -0800

    apply highlight and emphasis style

--- components/about.tsx
+++ components/about.tsx
@@ -19,16 +19,24 @@ export default function About() {
     >
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-3">
-        Greetings! I'm Jay, a fresh-faced tech graduate from the British
-        Columbia Institute of Technology with a passion for software innovation
-        and cloud computing. At SAP, as an Agile Developer Intern in the
-        platform infrastructure team, I engaged in developing microservices
-        running on diverse cloud platforms and honed my skills in DevOps through
-        updates and troubleshooting of virtual Linux machines. During my tenure
-        at IOTO, I embraced the full-stack developer role, where I crafted
-        sophisticated web scrapers, APIs, and user interfaces from the ground
-        up. I'm now seeking a full-time position where I can apply my diverse
-        skill set in a dynamic and innovative environment.
+        Greetings! I'm <span className="highlight">Jay</span>, a fresh-faced
+        tech graduate from the
+        <span className="highlight">
+          {" "}
+          British Columbia Institute of Technology
+        </span>{" "}
+        with a passion for software innovation and cloud computing. At{" "}
+        <span className="highlight">SAP</span>, as an
+        <span className="emphasis">Agile Developer Intern</span> in the platform
+        infrastructure team, I engaged in developing microservices running on
+        diverse cloud platforms and honed my skills in DevOps through updates
+        and troubleshooting of virtual Linux machines. During my tenure at
+        <span className="highlight"> IOTO</span>, I embraced the{" "}
+        <span className="emphasis">full-stack developer</span>
+        role, where I crafted sophisticated web scrapers, APIs, and user
+        interfaces from the ground up. I'm now seeking a{" "}
+        <span className="emphasis">full-time position</span> where I can apply
+        my diverse skill set in a dynamic and innovative environment.
       </p>
     </motion.section>
   );
